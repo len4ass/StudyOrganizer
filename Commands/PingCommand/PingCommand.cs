d4dcdf109511f61ef7f40c441309837ca26eb441
len4ass <whfromrussia@mail.ru>
2023-04-24 15:17:57 +0300

    Команды Ping и GetBirthdays

---
+++ Commands/PingCommand/PingCommand.cs
@@ -0,0 +1,38 @@
+﻿using StudyOrganizer.Models.User;
+using StudyOrganizer.Services.BotService;
+using StudyOrganizer.Services.BotService.Responses;
+using StudyOrganizer.Settings;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using BotCommand = StudyOrganizer.Services.BotService.Command.BotCommand;
+
+namespace PingCommand;
+
+public sealed class PingCommand : BotCommand
+{
+    public PingCommand()
+    {
+        Name = "ping";
+        Description = "Пингует на сервер.";
+        Format = "/ping";
+        Settings = new CommandSettings
+        {
+            AccessLevel = AccessLevel.Normal
+        };
+    }
+
+    public override async Task<UserResponse> ExecuteAsync(
+        ITelegramBotClient client,
+        Message message,
+        UserInfo userInfo,
+        IList<string> arguments)
+    {
+        var response = $"Ponged in chat: {message.Chat.Id}";
+
+        await BotMessager.Reply(
+            client,
+            message,
+            response);
+        return new UserResponse(response);
+    }
+}
